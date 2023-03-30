0c2d61c09bea9eb1e0dbe85f988a1653b92ee149
Wendy Liang <wendyl.y.123@gmail.com>
2023-03-30 02:05:00 -0700

    add basic fetch and display functionality

--- src/App.js
+++ src/App.js
@@ -1,56 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const MAXDISPLAYED = 3;
+  const [searchValue, setSearchValue] = useState('');
+  const [events, setEvents] = useState([]);
+  const [performers, setPerformers] = useState([]);
+  const [venues, setVenues] = useState([]);
+  useEffect(() => {
+    fetch(`https://mobile-staging.gametime.co/v1/search?q=${searchValue}`)
+      .then(response => response.json())
+      .then(data => {
+        const { events, performers, venues } = data;
+        setEvents(events.slice(0, MAXDISPLAYED));
+        setPerformers(performers.slice(0, MAXDISPLAYED));
+        setVenues(venues.slice(0, MAXDISPLAYED));
+      });
+  }, [searchValue]);
+
+  const searchResult = (id, image, title, subtitle) => (
+    <div key={id}>
+      <span><img src={image} alt={title} /></span>
+      <div>
+        <p>{title}</p>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>GAMETIME</h1>
+      <div className='search-container'>
+        <input 
+        type="text" 
+        value={searchValue} 
+        onChange={(input) => {
+          const value = input.target.value;
+          setSearchValue(value);
+        }} 
+        />
+        <div className='search-results'>
+          {searchValue && events?.map((event) => {
+            const name = event.event.name;
+            const img = event.performers[0].hero_image_url;
+            const subtitle = event.venue.name;
+            return searchResult(event.event.id, img, name, subtitle);
+          })}
+          {searchValue && performers?.map((performer) => {
+            return searchResult(
+              performer.id, 
+              performer.hero_image_url, 
+              performer.name,
+              performer.category,
+            );
+          })}
+          {searchValue && venues?.map((venue) => {
+            return searchResult(
+              venue.id,
+              venue.image_url,
+              venue.name,
+              venue.city,
+            );
+          })}
+        </div>
+      </div>
+      
     </div>
   );
 }
